00510cf82d9799e400318b9751c6988ba53c3e83
Lev_TheDev <levinosecretti@outlook.com>
2025-05-18 18:02:14 -0400

    Reuse instantiation.

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,6 +11,7 @@ public class Program {
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		// DaoFactory reterna para o sellerDao um sellerDaoJDBC com uma connection
 		
+		System.out.println("===== TEST 1: seller findById =====");
 		Seller seller = sellerDao.findById(3); 
 		// o sellerDao, que é sellerDaoJDBC dá um findById e retorna o seller
 		
